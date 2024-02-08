55ab7d24dce812b8f1bef2e6d0b0e01ad832d76a
Iryna <VacX_vb15@student.itstep.org>
2024-02-08 19:13:31 +0200

    hw-02.1 layout tic-tac-toe

--- app/src/main/java/com/example/java_android_201/MainActivity.java
+++ app/src/main/java/com/example/java_android_201/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.java_android_201;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
@@ -16,63 +17,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button b = findViewById(R.id.button);
-
-        b.setOnClickListener(v -> {
-
-            //#1
-            b.setWidth( b.getWidth() + 10 );
-
-            //#2
-            try {
-                int clicks_count = Integer.parseInt( (String)b.getText() );
-                if (clicks_count < 30) {
-                    b.setText( Integer.toString(clicks_count + 1) );
-                }
-                else {
-                    b.setText("Докликался.. X_X");
-                    b.setEnabled(false);
-                }
-            } catch (NumberFormatException e) {
-                throw new RuntimeException(e);
-            }
-        });
-
-        //#3
-        Button b3 = findViewById(R.id.button3);
-
-        LocalDateTime targetDateTime = LocalDateTime.of(2024, 2, 7, 8, 50);
-
-        b3.setOnClickListener(v -> {
-
-            Duration duration = Duration.between(LocalDateTime.now(), targetDateTime);
-
-            String msg = getFormattedTime(duration.toDays(), duration.toHours() % 24, duration.toMinutes() % 60, duration.getSeconds() % 60);
-
-            Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
-        });
 
     }
 
-    public static String getFormattedTime(long days, long hours, long minutes, long seconds) {
-        StringBuilder result = new StringBuilder();
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
 
-        if (days == 1) {
-            result.append(days).append(" day").append(" ");
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            setContentView(R.layout.activity_main_land);
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            setContentView(R.layout.activity_main);
         }
-        else if (days > 0) {
-            result.append(days).append(" days").append(" ");
-        }
-        if (hours > 0) {
-            result.append(hours).append("h").append(" ");
-        }
-        if (minutes > 0) {
-            result.append(minutes).append("m").append(" ");
-        }
-        if (seconds > 0) {
-            result.append(seconds).append("s");
-        }
-
-        return result.toString();
     }
 }
