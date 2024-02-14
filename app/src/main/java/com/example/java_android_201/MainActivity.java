364200e02fdcc9f075fff9f1e3febfd5f8261e62
Iryna <VacX_vb15@student.itstep.org>
2024-02-14 19:43:08 +0200

    hw-03 WishList -=- CustomAdapter

--- app/src/main/java/com/example/java_android_201/MainActivity.java
+++ app/src/main/java/com/example/java_android_201/MainActivity.java
@@ -1,185 +1,179 @@
 package com.example.java_android_201;
 
-import android.Manifest;
-import android.content.ContentResolver;
-import android.content.res.Configuration;
-import android.database.Cursor;
-import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.provider.ContactsContract;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.GridLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
-    private TicTacToe ticTacToe = new TicTacToe();
-    private List<Button> gameButtons = null;
-    private PlayerType currentPlayerType = PlayerType.Crosses;
-    private static boolean gameStarted = false;
-    private String colorZero = "#4B88AF";
-    private String colorCross = "#B15050";
-    private int winsCross = 0;
-    private int winsZero = 0;
-    private TextView scoreView;
-    private TextView turnView;
-    private Random random = new Random();
+class WishlistItem
+{
+    private boolean isChecked;
+    private int imageId;
+    private String title;
+    private double price;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        startGame();
+    public WishlistItem(String title, double price, int imageId) {
+        this.isChecked = false;
+        this.imageId = imageId;
+        this.title = title;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public int getImageId() {
+        return imageId;
+    }
+    public boolean isChecked() {
+        return isChecked;
+    }
+}
+
+class WishListAdapter extends RecyclerView.Adapter<WishListAdapter.WishlistViewHolder>
+{
+    private List<WishlistItem> wishList;
+    WishListAdapter(List<WishlistItem> wishList)
+    {
+        this.wishList = wishList;
     }
 
     @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-
-        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            setContentView(R.layout.activity_main_land);
-        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
-            setContentView(R.layout.activity_main);
-        }
-
-        initControls();
-        drawBoard();
+    public WishlistViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.my_item, parent, false);
+        return new WishlistViewHolder(itemView);
+    }
+    @Override
+    public void onBindViewHolder(WishlistViewHolder holder, int position) {
+        WishlistItem wishlistItem = wishList.get(position);
+        holder._checkBox.setChecked(wishlistItem.isChecked());
+        holder._image.setImageResource(wishlistItem.getImageId());
+        holder._title.setText(wishlistItem.getTitle());
+        holder._price.setText(Double.toString(wishlistItem.getPrice()));
+    }
+    @Override
+    public int getItemCount() {
+        return wishList.size();
     }
 
-    public void initControls() {
-        int size = ticTacToe.getBoardSize();
-        gameButtons = new ArrayList<>();
-        gameButtons.add(findViewById(R.id.button1));
-        gameButtons.add(findViewById(R.id.button2));
-        gameButtons.add(findViewById(R.id.button3));
-        gameButtons.add(findViewById(R.id.button4));
-        gameButtons.add(findViewById(R.id.button5));
-        gameButtons.add(findViewById(R.id.button6));
-        gameButtons.add(findViewById(R.id.button7));
-        gameButtons.add(findViewById(R.id.button8));
-        gameButtons.add(findViewById(R.id.button9));
+    class WishlistViewHolder extends RecyclerView.ViewHolder
+    {
+        GridLayout _gridlayout;
+        CheckBox _checkBox;
+        ImageView _image;
+        TextView _title;
+        TextView _price;
 
-        for(int i = 0; i < size; i++) {
-            for(int j = 0; j < size; j++) {
-                Button btn = gameButtons.get((i * size) + j);
-                btn.setOnClickListener(this::onClick);
-                GameButtonTag tag = new GameButtonTag();
-                tag.setX(j);
-                tag.setY(i);
-                btn.setTag(tag);
-            }
+        WishlistViewHolder(View view)
+        {
+            super(view);
+            _gridlayout = view.findViewById(R.id.griditem);
+            _checkBox = view.findViewById(R.id.checkbox);
+            _image = view.findViewById(R.id.image);
+            _title = view.findViewById(R.id.tv_title);
+            _price = view.findViewById(R.id.tv_price);
+
+            _checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+                {
+                    if (isChecked)
+                    {
+                        _gridlayout.setBackgroundResource(R.drawable.rounded_border_2);
+                    }
+                    else
+                    {
+                        _gridlayout.setBackgroundResource(R.drawable.rounded_border);
+                    }
+                    TextView totalSumTextView = view.getRootView().findViewById(R.id.tv_totalsum);
+                    double totalSum = ChangeTotalSum(String.valueOf(totalSumTextView.getText()), isChecked);
+                    totalSumTextView.setText("Total : ₴" + String.format("%.2f", totalSum));
+                }
+            });
+
         }
-
-        scoreView = findViewById(R.id.tv_score);
-        turnView = findViewById(R.id.tv_turn);
-
-        findViewById(R.id.button_newgame).setOnClickListener((view) -> startGame());
-    }
-
-    public void startGame() {
-        ticTacToe.resetBoard();
-        gameStarted = true;
-        currentPlayerType = PlayerType.Crosses;
-
-        initControls();
-        this.drawBoard();
-    }
-
-    public void drawBoard() {
-        int[][] board = ticTacToe.getGameBoard();
-        int size = ticTacToe.getBoardSize();
-
-        for(int i = 0; i < size; i++) {
-            for(int j = 0; j < size; j++) {
-                PlayerType elem = ticTacToe.getType(board[i][j]);
-                String buttonText = elem == PlayerType.Crosses ? "X" : elem == PlayerType.Zeroes ? "O" : "";
-                String buttonTextColor = elem == PlayerType.Crosses ? colorCross : colorZero;
-                Button btn = gameButtons.get((i * size) + j);
-                btn.setText(buttonText);
-                btn.setTextColor(Color.parseColor(buttonTextColor));
+        public double ChangeTotalSum(String text, boolean isMinus)
+        {
+            String[] parts = text.split("₴");
+            double res = Double.parseDouble(parts[1].replace(",", "."));
+            if (isMinus)
+            {
+                res -= Double.parseDouble((String) _price.getText());
             }
-        }
-
-        String playerTypeStr = currentPlayerType == PlayerType.Crosses ? "X" : "O";
-        scoreView.setText(String.format("%d:%d", winsCross, winsZero));
-        turnView.setText(ticTacToe.isDraw() ? "Draw" : String.format("%s's turn", playerTypeStr));
-    }
-
-    public void onClick(View view) {
-        System.out.println(gameStarted);
-        if(!gameStarted) return;
-
-        Button btn = (Button)view;
-        GameButtonTag tag = (GameButtonTag)btn.getTag();
-
-        if(ticTacToe.getItem(tag.getY(), tag.getX()) != PlayerType.Empty) {
-            return;
-        }
-
-        PlayerType winner = ticTacToe.setItem(currentPlayerType, tag.getY(), tag.getX());
-
-        if(currentPlayerType == PlayerType.Crosses) {
-            if(winner != null) {
-                winsCross++;
-                gameStarted = false;
+            else
+            {
+                res += Double.parseDouble((String) _price.getText());
             }
-            else {
-                currentPlayerType = PlayerType.Zeroes;
-            }
-        }
-        else {
-            if(winner != null) {
-                winsZero++;
-                gameStarted = false;
-            }
-            else {
-                currentPlayerType = PlayerType.Crosses;
-            }
-        }
-
-        drawBoard();
-    }
-
-    public void botTurn() {
-        if(!gameStarted) return;
-        int size = ticTacToe.getBoardSize();
-
-        for (int i = 0; i < size * size; i++) {
-            int y = random.nextInt(size);
-            int x = random.nextInt(size);
-
-            if(ticTacToe.getItem(y, x) == PlayerType.Empty) {
-                onClick(gameButtons.get((y * size) + x));
-                break;
-            }
+            return res;
         }
     }
 }
 
-class GameButtonTag {
-    private int x;
-    private int y;
+public class MainActivity extends AppCompatActivity {
+    private List<WishlistItem> list = new ArrayList<>();
+    private WishListAdapter a;
 
-    public int getX() {
-        return x;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView rv = findViewById(R.id.recyclerView);
+
+        a = new WishListAdapter(list);
+        RecyclerView.LayoutManager lm = new LinearLayoutManager(getApplicationContext());
+        rv.setLayoutManager(lm);
+        rv.setItemAnimator(new DefaultItemAnimator());
+        rv.setAdapter(a);
+
+        prepareWishlistItemData();
+
+        double totalSum = calculateTotalSum();
+        TextView totalSumTextView = findViewById(R.id.tv_totalsum);
+        totalSumTextView.setText("Total : ₴" + String.format("%.2f", totalSum));
     }
 
-    public void setX(int x) {
-        this.x = x;
+    private void prepareWishlistItemData()
+    {
+        list.add(new WishlistItem("Автомобильная утка у шоломі Шалена качка", 199, R.drawable.duck));
+        list.add(new WishlistItem("Танцюючий Кактус повторюшка", 585, R.drawable.cactus));
+        list.add(new WishlistItem("Воскреситель: Анатомія фантастичних істот - Е. Б. Гадспет", 480, R.drawable.book));
+        list.add(new WishlistItem("Риба м'яка, лосось 20см КР65", 60, R.drawable.fish));
+        list.add(new WishlistItem("Цукерки глазуровані Roshen Konafetto Bianco 1 кг", 247, R.drawable.candies));
+
+        a.notifyDataSetChanged();
     }
 
-    public int getY() {
-        return y;
-    }
+    private double calculateTotalSum()
+    {
+        double totalSum = 0.0;
 
-    public void setY(int y) {
-        this.y = y;
+        for (WishlistItem item : list)
+        {
+            if(!item.isChecked())
+            {
+                totalSum += item.getPrice();
+            }
+        }
+
+        return totalSum;
     }
 }
--- app/src/main/java/com/example/java_android_201/PlayerType.java
+++
@@ -1,7 +0,0 @@
-package com.example.java_android_201;
-
-public enum PlayerType {
-    Crosses,
-    Zeroes,
-    Empty
-}
